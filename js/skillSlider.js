492d9eaf7d5da406494ab1b3ee9ebe41d133b343
hyeongkyuPark <phg0644@gmail.com>
2021-06-15 15:41:42 +0900

    info-section UI complete

---
+++ js/skillSlider.js
@@ -0,0 +1,36 @@
+(function() {
+    const skillSlider = document.querySelector('.skill-slide');
+    const skillList = skillSlider.querySelector('.skill-list');
+    const leftBtn = skillSlider.querySelector('.arrow.left');
+    const rightBtn = skillSlider.querySelector('.arrow.right');
+
+    const skillCount = skillList.children.length;
+    const current = {
+        moveX: 0,
+    };
+
+    function moveLeft() {
+        if(current.moveX <= 0) {
+            return;
+        }
+        console.log(current.moveX);
+        current.moveX -= 100;
+        skillList.style.transform = `translateX(-${current.moveX}%)`;
+    }
+
+    function moveRight() {
+        if(current.moveX >= 100 * (skillCount-1)) {
+            return;
+        }
+        console.log(current.moveX);
+        current.moveX += 100;
+        skillList.style.transform = `translateX(-${current.moveX}%)`;
+    }
+
+    function init() {
+        leftBtn.addEventListener('click', moveLeft);
+        rightBtn.addEventListener('click', moveRight);
+    }
+
+    init();
+})();
